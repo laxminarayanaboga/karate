55b4a05bc67cef65565a34a75e6c495343ae0380
Sandhya <Sandhya@192.168.0.100>
2022-04-22 23:48:37 +0100

    Moved to Junit5 - a stable version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # karate
+
+This is a sample Java project on how to use Karate test automation framework. 
+
+Sample APIs: https://reqres.in/
+
+# Running
+command line: 
+mvn test -Dtest=TestRunnerWithJunit5#testAll
---
+++ src/test/java/tests/TestRunnerWithJunit5.java
@@ -0,0 +1,33 @@
+package tests;
+
+import com.intuit.karate.junit5.Karate;
+
+public class TestRunnerWithJunit5 {
+
+	@Karate.Test
+	Karate testSample() {
+		return Karate.run("sample").relativeTo(getClass());
+	}
+
+	@Karate.Test
+	Karate testTags() {
+		return Karate.run("tags").tags("@second").relativeTo(getClass());
+	}
+
+	@Karate.Test
+	Karate testFullPath() {
+		return Karate.run("classpath:karate/tags.feature").tags("@first");
+	}
+
+	@Karate.Test
+	Karate testSystemProperty() {
+		return Karate.run("classpath:karate/tags.feature").tags("@second").karateEnv("e2e").systemProperty("foo",
+				"bar");
+	}
+
+	@Karate.Test
+	Karate testAll() {
+		return Karate.run().relativeTo(getClass());
+	}
+
+}
